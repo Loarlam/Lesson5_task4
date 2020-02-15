7c67ae5af2babd23e6e8e0145f004ce38c00f1de
Loarlam <messiah02@mail.ru>
2020-02-15 14:33:56 +0300

    Add project files.

---
+++ Task4/Article.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task4
+{
+    class Article
+    {
+        readonly string[] _productName, _shopName;
+        const double _tableCost = 1500.30, _tvCost = 3320.50, _monitorCost = 2230;
+
+        public Article(params string[] listOfInformationFromStore)
+        {
+            _productName = listOfInformationFromStore[0..3];
+            _shopName = listOfInformationFromStore[3..6];
+        }
+        //public Article(string productName, string shopName)
+        //{
+        //    _productName = productName;
+        //    _shopName = shopName;
+        //}
+
+        public string this[string index]
+        {
+            get
+            {
+                if (_productName[0] == index)
+                    return $"{_productName[0]} расположен в {_shopName[0]}. Цена {_tableCost}";
+                else if (_productName[1] == index)
+                    return $"{_productName[1]} расположен в {_shopName}. Цена {_tvCost}";
+                else if (_productName[2] == index)
+                    return $"{_productName[2]} расположен в {_shopName[2]}. Цена {_monitorCost}";
+                else
+                    return $"{index} отсутствует в магазинах.";
+            }
+        }
+    }
+}
---
+++ Task4/Store.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task4
+{
+    class Store
+    {
+        Article _articles;
+        readonly string _selectionFromCatalog;
+
+        public Store(string selectionFromCatalog)
+        {
+            _selectionFromCatalog = selectionFromCatalog;
+        }
+
+        public void UstanovkaAndVivodNaEkran()
+        {
+            _articles = new Article("Стол", "Телевизор", "Монитор", "Marble", "Islor", "Krostler");
+
+            switch (_selectionFromCatalog)
+            {
+                case "Стол":
+                case "1914":
+                    Console.WriteLine($"Ваш {_articles["Стол"]}");
+                    break;
+                case "Телевизор":
+                case "1517":
+                    Console.WriteLine($"Ваш {_articles["Телевизор"]}");
+                    break;
+                case "Монитор":
+                case "1433":
+                    Console.WriteLine($"Ваш {_articles["Монитор"]}");
+                    break;
+                default:
+                    Console.WriteLine($"{_articles[_selectionFromCatalog]}");
+                    break;
+            }
+        }
+    }
+}
